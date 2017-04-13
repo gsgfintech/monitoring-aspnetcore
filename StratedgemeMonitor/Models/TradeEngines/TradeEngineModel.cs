025dcac2dd89a78e005919baec1352d75c380cc6
Guillaume Teirlinck <guillaume.teirlinck@gsgfintech.com>
2017-04-13 17:35:04 +0800

    New trade engine control (wip)

--- StratedgemeMonitor/Models/TradeEngines/TradeEngineModel.cs
+++ StratedgemeMonitor/Models/TradeEngines/TradeEngineModel.cs
@@ -19,6 +19,7 @@ namespace StratedgemeMonitor.Models.TradeEngines
         public SelectList CrossesList { get; private set; }
         public SelectList TradingCrossesList { get; private set; }
         public SelectList NonTradingCrossesList { get; private set; }
+        public SelectList AllStratsList { get; private set; }
         public SelectList ActiveStratsList { get; private set; }
         public SelectList InactiveStratsList { get; private set; }
         public SelectList TradingStratsList { get; private set; }
@@ -36,6 +37,7 @@ namespace StratedgemeMonitor.Models.TradeEngines
                 TradingCrossesList = new SelectList((new string[1] { "ALL" }).Concat(Strats.Where(s => s.Trading).Select(s => s.Cross.ToString()).OrderBy(c => c)));
                 NonTradingCrossesList = new SelectList((new string[1] { "ALL" }).Concat(Strats.Where(s => !s.Trading).Select(s => s.Cross.ToString()).OrderBy(c => c)));
 
+                AllStratsList = new SelectList((new string[1] { "ALL" }).Concat(Strats.Select(s => $"{s.Name}-{s.Version}").OrderBy(s => s)));
                 ActiveStratsList = new SelectList(Strats.Where(s => s.Active).Select(s => $"{s.Name}-{s.Version}").OrderBy(s => s));
                 InactiveStratsList = new SelectList(Strats.Where(s => !s.Active).Select(s => $"{s.Name}-{s.Version}").OrderBy(s => s));
                 TradingStratsList = new SelectList(Strats.Where(s => s.Trading).Select(s => $"{s.Name}-{s.Version}").OrderBy(s => s));
